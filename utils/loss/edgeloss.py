9189d4b73df63f514812edc71efd19551c43df58
MR3z4 <mohammadzade.m.r@gmail.com>
2021-05-02 00:37:01 +0430

    added weight to edge loss.

--- utils/loss/edgeloss.py
+++ utils/loss/edgeloss.py
@@ -16,9 +16,9 @@ class EdgeLoss(nn.Module):
 
     def forward(self, preds, target):
         preds_max = soft_argmax(preds)
-        preds_max /= (preds_max+1e-32)
+        preds_max /= (preds_max + 1e-32)
         # preds_max = (preds_max > 0) * 1
         # preds_edge = generate_edge_tensor(preds_max)
-        preds_edge = kornia.filters.sobel(preds_max.unsqueeze(1)*1.7888, eps=1e-32).squeeze()
-        loss = self.criterion(preds_edge, target)
+        preds_edge = kornia.filters.sobel(preds_max.unsqueeze(1) * 1.7888, eps=1e-32).squeeze()
+        loss = 0.1 * self.criterion(preds_edge, target) + 0.9 * self.criterion((1 - preds_edge), (1 - target))
         return loss
